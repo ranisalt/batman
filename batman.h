d89be7383a54649de603e60b557e2d12e2c7f600
Ranieri Althoff <ranisalt@gmail.com>
2017-05-02 00:04:19 -0300

    Remove code duplication

--- batman.h
+++ batman.h
@@ -24,43 +24,37 @@ public:
     template<class PowerUnit = watts>
     PowerUnit energy_full() const
     {
-        std::stringstream filename;
-        filename << sys_path << "energy_full";
-
-        std::ifstream file{filename.str()};
-        int mw;
-        file >> mw;
-        return power_unit_cast<PowerUnit>(milliwatts{mw});
+        return read_mw_from_int<PowerUnit>("energy_full");
     }
 
     template<class PowerUnit = watts>
     PowerUnit energy_full_design() const
     {
-        std::stringstream filename;
-        filename << sys_path << "energy_full_design";
-
-        std::ifstream file{filename.str()};
-        int mw;
-        file >> mw;
-        return power_unit_cast<PowerUnit>(milliwatts{mw});
+        return read_mw_from_int<PowerUnit>("energy_full_design");
     }
 
     template<class PowerUnit = watts>
     PowerUnit energy_now() const
     {
-        std::stringstream filename;
-        filename << sys_path << "energy_now";
-
-        std::ifstream file{filename.str()};
-        int mw;
-        file >> mw;
-        return power_unit_cast<PowerUnit>(milliwatts{mw});
+        return read_mw_from_int<PowerUnit>("energy_now");
     }
 
 private:
     power_supply(std::string sys_path) :
             sys_path{std::move(sys_path)}
     {}
+
+    template<class PowerUnit = watts>
+    PowerUnit read_mw_from_int(const std::string& filename) const
+    {
+        std::stringstream ss;
+        ss << sys_path << filename;
+
+        std::ifstream file{ss.str()};
+        int mw;
+        file >> mw;
+        return power_unit_cast<PowerUnit>(milliwatts{mw});
+    }
 
     std::string sys_path;
 };
